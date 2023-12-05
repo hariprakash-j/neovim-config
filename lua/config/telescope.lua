d2551274d6de857ec19efc95b983f34588fd4f9d
hariprakash-j <73071214+hariprakash-j@users.noreply.github.com>
2023-12-05 13:32:08 +0530

    feat: add auto format

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,19 +1,19 @@
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
-local telescope = require('telescope')
+local telescope = require("telescope")
 
-telescope.setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
-}
+telescope.setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-u>"] = false,
+				["<C-d>"] = false,
+			},
+		},
+	},
+})
 
 -- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
+pcall(require("telescope").load_extension, "fzf")
 
 return telescope
